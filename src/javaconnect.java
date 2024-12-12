49124fc38a8c6fd08a6944914df94cd10660563f
Mohamed Saeed <msiadg54@gmail.com>
2024-12-12 20:49:16 +0200

    add IssueBook frame

--- src/javaconnect.java
+++ src/javaconnect.java
@@ -11,14 +11,16 @@ import java.sql.*;
 import javax.swing.JOptionPane;
 
 public class javaconnect {
+
     Connection conn;
+
     public static Connection ConnecrDb() {
         try {
 
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarynew","root","1230mody");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarynew", "root", "root");
 
             return conn;
-        }catch(Exception e) {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
             return null;
         }
